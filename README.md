0933ddd1fd1e5a1c82dcba67e0615a27bd281ba0
Austin Lane <austinlane@Austins-Air.fios-router.home>
2022-04-27 16:05:47 -0400

    prettier cleanup

--- README.md
+++ README.md
@@ -3,11 +3,14 @@ Clover Take Home for Austin Infante Lane
 How to run:
 
 npm/npx
+
 ```bash
 npm install
 npx cypress open
 ```
+
 yarn
+
 ```bash
 yarn install
 yarn run cypress open
--- cypress/integration/0-clover-take-home/search.spec.js
+++ cypress/integration/0-clover-take-home/search.spec.js
@@ -1,21 +1,19 @@
-describe('Google Search', () => {
-    const url = 'https://www.google.com'
+describe("Google Search", () => {
+  const url = "https://www.google.com";
 
-    beforeEach(() => {
-        cy.visit(url)
-    })
+  beforeEach(() => {
+    cy.visit(url);
+  });
 
-    it('When a term is searched it should be in the first result', () => {
-        const input = 'clover' 
-        const index = 0
+  it("When a term is searched it should be in the first result", () => {
+    const input = "clover";
+    const index = 0;
 
-        const search = cy.get('input[title=Search]')
-		search.type(`${input}{enter}`)
+    const search = cy.get("input[title=Search]");
+    search.type(`${input}{enter}`);
 
-        const results = cy.get('#search')
-            .find('a > h3')
-            .filter(':visible')
-        
-        results.eq(index).contains(input, { matchCase: false })
-    })
-})
+    const results = cy.get("#search").find("a > h3").filter(":visible");
+
+    results.eq(index).contains(input, { matchCase: false });
+  });
+});
